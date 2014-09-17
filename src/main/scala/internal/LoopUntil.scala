6652543e82135853d12cbdca430401fe92c2bc65
jeansung <jsung@hmc.edu>
2014-09-17 14:44:41 -0700

    finished loop_until, in progress for repeat until
    still a work in progress

--- src/main/scala/internal/LoopUntil.scala
+++ src/main/scala/internal/LoopUntil.scala
@@ -10,14 +10,21 @@ package internal
  */
 object LoopUntil extends App {
 
-  // define the new control-flow structure here
 
-  var i = 0
+    def loop_until(breakCondition: => Boolean)(body: => Unit) = {
+    while (!breakCondition) {
+      body
+    }
+  }
   
+    
+    // is reevaluate the boolean 
+  var i = 0
   loop_until (i > 9) {
       if ( (i % 2) == 0 )
           println(i)
       i += 1
   }
+  
 
 }
--- src/main/scala/internal/RepeatUntil.scala
+++ src/main/scala/internal/RepeatUntil.scala
@@ -10,14 +10,40 @@ package internal
  */
 
 object RepeatUntil extends App {
-  
-  // define the new control-flow structure here
+	 def repeat (body: => Unit) {
+		body 
+		while (!until _) {body}
+	    def until(condition: Boolean) = if (condition) true else false
+	   
+	 }
+	
+	 
+	//  make the until similar to a function to mimic the function call 
+	  // define the new control-flow structure here
+
+	  	
+	  
+//      def repeat (body: => Unit)(breakCondition: => Boolean) = {
+//        body
+//
+//    while (!breakCondition) {
+//      body
+//    }
+ // }
+      
+      //def until(condition: Boolean) = if (condition) true else false 
+      //val until = until2 _
+      
+    //val until = (condition: Boolean) => if (condition) true else false 
+      
 
   var i = 0
   repeat  {
       if ( (i % 2) == 0 )
           println(i)
       i += 1
-  } until(i > 9)        
+  } until(i > 9)    
+  
+  
 }
 
